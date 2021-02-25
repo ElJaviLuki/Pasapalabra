3a1a03b29a0882c2c1ba78917ca974adbc001296
ElJaviLuki <eljaviluki@gmail.com>
2021-02-25 19:18:56 +0100

    Added 'tests' project for general testing

---
+++ tests/src/tests.cpp
@@ -0,0 +1,23 @@
+/*
+	tests.cpp
+	F. Javier L. Cacenave - @ElJaviLuki
+
+	Do here the tests you consider.
+
+	1. To test a section from a specific project, do:
+		1.1.	Add -if it is not added- the project you want to look to the References ('tests' -> References (right click) -> Add reference...)
+		1.2.	Copy all the 'Additional Include Directories' -if it is not- from that project to the one from the project 'tests'.
+		1.3.	If using static libraries (i.e. .lib file), you will also need to add the library -if it is not- to the linker input.
+		1.4.	#include here all the header files you need, using the path relative to the 'Additional Include Directories' path.
+
+	2. Now do the tests you consider.
+
+	3. You're free! If you think your test is important enough to be used in a significative number of situations and/or can be helpful
+	for further	developing this project, consider implementing it in a new source file for a commit; otherwise, clean your test code once
+	you're done	and don't ever commit it!
+*/
+
+int main()
+{
+	return 0;
+}
